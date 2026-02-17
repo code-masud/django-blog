f1f185fcb6bac20a2b52d65fc1d5c508b0b47dbb
code-masud <code.masud@hotmail.com>
2026-02-17 13:46:48 +0600

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,28 +1,26 @@
+
 from django.contrib import admin
-from django.db.models.query import QuerySet
-from django.forms import ModelForm
-from django.http import HttpRequest
 from .models import Category, Tag, Comment, Post
-from django.utils import timezone
 from django.utils.html import format_html
+from config.admin import AuditAdminMixin
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(AuditAdminMixin, admin.ModelAdmin):
     model = Post
 
-    list_display = ['id', 'preview_image', 'title', 'status', 'created_by', 'created_at', 'updated_by', 'updated_at', 'is_deleted', 'deleted_by', 'deleted_at']
-    list_display_links = ['id', 'title']
-    list_filter = ['published_at', 'status']
+    list_display = ('id', 'preview_image', 'title', 'status')
+    list_display_links = ('id', 'title')
+    list_filter = ('published_at', 'status')
     list_per_page = 10
-    search_fields = ['title', 'slug', 'content']
-    filter_horizontal = ['categories', 'tags']
-    ordering = ['-published_at']
+    search_fields = ('title', 'slug', 'content')
+    filter_horizontal = ('categories', 'tags')
+    ordering = ('-published_at',)
 
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author', 'created_by', 'updated_by', 'deleted_by')
     date_hierarchy = 'published_at'
     readonly_fields = ('created_at', 'updated_at')
-    actions = ['published_posts', 'restore_posts']
+    actions = ('published_posts', )
 
     fieldsets = (
         ('Main Content', {
@@ -48,9 +46,7 @@ class PostAdmin(admin.ModelAdmin):
         })
     )
 
-    def get_queryset(self, request):
-        return Post.all_objects.all()
-
+    @admin.display(description='Image')
     def preview_image(self, obj):
         if obj.featured_image:
             return format_html(
@@ -58,41 +54,24 @@ class PostAdmin(admin.ModelAdmin):
                 obj.featured_image.url
             )
         return '--'
-    preview_image.short_description = 'Image'
-
+    
+    @admin.action(description='Published selected Posts')
     def published_posts(self, request, queryset):
-        queryset.update(status=Post.Status.PUBLISHED)
-    published_posts.short_description = 'Published selected Posts'
+        qs = queryset.exclude(status=Post.Status.PUBLISHED)
+        count = qs.update(status=Post.Status.PUBLISHED)
+        self.message_user(request, f'{count} posts status updated successfully.')
 
-    def restore_posts(self, request, queryset):
-        queryset.update(is_deleted=False, deleted_at=None, deleted_by=None)
-    restore_posts.short_description = 'Restore selected Posts'
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.author = request.user
-            obj.created_by = request.user
-        else:
-            obj.updated_by = request.user
-        return super().save_model(request, obj, form, change)
-    
-    def delete_model(self, request, obj):
-        obj.delete(user=request.user)
-    
-    def delete_queryset(self, request: HttpRequest, queryset: QuerySet) -> None:
-        for obj in queryset:
-            obj.delete(user=request.user)
-    
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(AuditAdminMixin, admin.ModelAdmin):
     model = Category
 
-    list_display = ['id', 'name', 'slug', 'description', 'is_active']
-    list_display_links = ['id', 'name']
-    list_filter = ['is_active']
+    list_display = ('id', 'name', 'slug', 'description', 'is_active')
+    list_display_links = ('id', 'name')
+    list_filter = ('is_active',)
     list_per_page = 10
-    search_fields = ['name', 'slug', 'description']
-    ordering = ['name']
+    search_fields = ('name', 'slug', 'description')
+    ordering = ('name',)
 
     prepopulated_fields = {'slug': ('name',)}
 
@@ -101,27 +80,17 @@ class CategoryAdmin(admin.ModelAdmin):
             'fields': ('name', 'slug', 'description', 'is_active')
         }),
     )
-
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-        else:
-            obj.updated_by = request.user
-        return super().save_model(request, obj, form, change)
-    
-    def delete_model(self, request, obj):
-        obj.delete(user=request.user)
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(AuditAdminMixin, admin.ModelAdmin):
     model = Tag
 
-    list_display = ['id', 'name', 'slug', 'description', 'is_active']
-    list_display_links = ['id', 'name']
-    list_filter = ['is_active']
+    list_display = ('id', 'name', 'slug', 'description', 'is_active')
+    list_display_links = ('id', 'name')
+    list_filter = ('is_active',)
     list_per_page = 10
-    search_fields = ['name', 'slug', 'description']
-    ordering = ['name']
+    search_fields = ('name', 'slug', 'description')
+    ordering = ('name',)
 
     prepopulated_fields = {'slug': ('name',)}
 
@@ -131,27 +100,17 @@ class TagAdmin(admin.ModelAdmin):
         }),
     )
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-        else:
-            obj.updated_by = request.user
-        return super().save_model(request, obj, form, change)
-    
-    def delete_model(self, request, obj):
-        obj.delete(user=request.user)
-
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(AuditAdminMixin, admin.ModelAdmin):
     model = Comment
 
-    list_display = ['id', 'short_text', 'post', 'user', 'is_approved']
-    list_display_links = ['id', 'post']
-    list_filter = ['is_approved']
+    list_display = ('id', 'short_text', 'post', 'user', 'is_approved')
+    list_display_links = ('id', 'post')
+    list_filter = ('is_approved',)
     list_per_page = 10
-    search_fields = ['post__title', 'user__username', 'text']
-    ordering = ['post']
-    actions = ['approve_comments']
+    search_fields = ('post__title', 'user__username', 'text')
+    ordering = ('post',)
+    actions = ('approve_comments',)
 
     fieldsets = (
         ('Main Content', {
@@ -159,20 +118,10 @@ class CommentAdmin(admin.ModelAdmin):
         }),
     )
 
+    @admin.display(description='Comment')
     def short_text(self, obj):
         return obj.text[:10] + '...' if len(obj.text) > 10 else obj.text
-    short_text.short_description = 'Comment'
 
+    @admin.display(description='Approve selected comments')
     def approve_comments(self, request, queryset):
         queryset.update(is_approved=True)
-    approve_comments.short_description = 'Approve selected comments'
-
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-        else:
-            obj.updated_by = request.user
-        return super().save_model(request, obj, form, change)
-    
-    def delete_model(self, request, obj):
-        obj.delete(user=request.user)
