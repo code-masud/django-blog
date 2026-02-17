407371dd4022068e426d1135083bc4c8df5b1a73
code-masud <code.masud@hotmail.com>
2026-02-17 22:21:21 +0600

    Update upload_path.py

--- services/upload_path.py
+++ services/upload_path.py
@@ -2,6 +2,12 @@ import os
 import uuid
 from django.utils.text import slugify
 
+def seo_upload_path(instance, filename):
+    root, ext =os.path.splitext(filename)
+    unique_identity = slugify(filename) + '_' + uuid.uuid4().hex
+    filename = f'{unique_identity}{ext}'
+    return os.path.join('seo', filename)
+
 def logo_upload_path(instance, filename):
     root, ext =os.path.splitext(filename)
     unique_identity = slugify(filename) + '_' + uuid.uuid4().hex
@@ -14,8 +20,8 @@ def avatar_upload_path(instance, filename):
     filename = f'{unique_identity}{ext}'
     return os.path.join('avatar', filename)
 
-def post_upload_path(instance, filename):
+def article_upload_path(instance, filename):
     root, ext =os.path.splitext(filename)
     unique_identity = slugify(filename) + '_' + uuid.uuid4().hex
     filename = f'{unique_identity}{ext}'
-    return os.path.join('post', filename)
+    return os.path.join('article', filename)
