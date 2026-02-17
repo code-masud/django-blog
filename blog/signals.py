32a10a21ead94ce2d96073dbf9197c7d9766f211
code-masud <code.masud@hotmail.com>
2026-02-17 22:21:11 +0600

    Update signals.py

--- blog/signals.py
+++ blog/signals.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.core.files.storage import default_storage
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
-from .models import Post
+from .models import Article
 
 def delete_file(file=None):
     if not file:
@@ -11,15 +11,15 @@ def delete_file(file=None):
     if default_storage.exists(file.name):
         default_storage.delete(file.name)
 
-@receiver(pre_save, sender=Post)
+@receiver(pre_save, sender=Article)
 def delete_old_image_on_change(sender, instance, **kwargs):
     if not instance.pk:
         return
     
-    old_obj = get_object_or_404(Post, pk=instance.pk)
+    old_obj = get_object_or_404(Article, pk=instance.pk)
     if old_obj.featured_image and old_obj.featured_image != instance.featured_image:
         delete_file(old_obj.featured_image)
 
-@receiver(post_delete, sender=Post)
+@receiver(post_delete, sender=Article)
 def delete_image_on_delete(sender, instance, **kwargs):
     delete_file(getattr(instance,'featured_image', None))
