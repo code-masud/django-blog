81f4dfb1eae98d9c43d7373a151115245ba2d6fa
code-masud <code.masud@hotmail.com>
2026-02-17 22:21:08 +0600

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.db.models import Q
 from django.utils.text import slugify
 from services.image_validator import image_validation
-from services.upload_path import post_upload_path
+from services.upload_path import article_upload_path
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.core.validators import MinLengthValidator
 from config.models.mixins import AuditSoftDeleteMixin as SoftDeleteMixin
+from config.models.mixins import SEOMixin
 
-class Category(SoftDeleteMixin, models.Model):
+class Category(SoftDeleteMixin, SEOMixin, models.Model):
     name = models.CharField(max_length=200, validators=[MinLengthValidator(3)])
     slug = models.SlugField(max_length=200, validators=[MinLengthValidator(3)])
     description = models.TextField(help_text='Short description')
@@ -18,7 +19,7 @@ class Category(SoftDeleteMixin, models.Model):
         verbose_name_plural = 'Categories'
         ordering = ['name']
         constraints = [
-            models.UniqueConstraint(fields=['name', 'slug'], condition=Q(is_active=True), name='category_unique_name_slug')
+            models.UniqueConstraint(fields=['name', 'slug'], condition=Q(is_active=True), name='%(app_label)s_%(class)s_unique_name_slug')
         ]
         indexes = [
             models.Index(fields=['is_active', 'name']),
@@ -28,12 +29,15 @@ class Category(SoftDeleteMixin, models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse_lazy("blog:category_detail", args=[self.slug])
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
 
-class Tag(SoftDeleteMixin, models.Model):
+class Tag(SoftDeleteMixin, SEOMixin, models.Model):
     name = models.CharField(max_length=200, validators=[MinLengthValidator(3)])
     slug = models.SlugField(max_length=200, validators=[MinLengthValidator(3)])
     description = models.TextField(help_text='Short description')
@@ -43,7 +47,7 @@ class Tag(SoftDeleteMixin, models.Model):
         verbose_name_plural = 'Tags'
         ordering = ['name']
         constraints = [
-            models.UniqueConstraint(fields=['name', 'slug'], condition=Q(is_active=True), name='tag_unique_name_slug')
+            models.UniqueConstraint(fields=['name', 'slug'], condition=Q(is_active=True), name='%(app_label)s_%(class)s_unique_name_slug')
         ]
         indexes = [
             models.Index(fields=['is_active', 'name']),
@@ -53,12 +57,15 @@ class Tag(SoftDeleteMixin, models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse_lazy("blog:tag_detail", args=[self.slug])
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
     
-class Post(SoftDeleteMixin, models.Model):
+class Article(SoftDeleteMixin, SEOMixin, models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DR', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -66,30 +73,25 @@ class Post(SoftDeleteMixin, models.Model):
     
     title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
     slug = models.SlugField(max_length=200, validators=[MinLengthValidator(5)])
-    excerpt = models.TextField(help_text="Brief summary for listings", max_length=300)
     content = models.TextField()
-    featured_image = models.ImageField(upload_to=post_upload_path, blank=True, null=True, validators=[image_validation])
+    featured_image = models.ImageField(upload_to=article_upload_path, blank=True, null=True, validators=[image_validation])
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
-    # Technical specifications for label projects
-    substrate = models.CharField(max_length=100, blank=True, help_text="Label material")
-    printing_technology = models.CharField(max_length=100, blank=True)
-    finishing_techniques = models.CharField(max_length=200, blank=True)
+    categories = models.ManyToManyField(Category, related_name='articles', blank=True)
+    tags = models.ManyToManyField(Tag, related_name='articles', blank=True)
 
-    categories = models.ManyToManyField(Category, related_name='posts', blank=True)
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
-
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
     published_at = models.DateTimeField(blank=True, null=True)
 
-    meta_title = models.CharField(max_length=200, blank=True)
-    meta_description = models.TextField(max_length=300, blank=True)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
 
     class Meta:
-        verbose_name_plural = 'Posts'
+        verbose_name_plural = 'Articles'
         ordering = ['-published_at']
         constraints = [
-            models.UniqueConstraint(fields=['title', 'slug'], condition=Q(status='PB'), name='post_unique_name_slug')
+            models.UniqueConstraint(fields=['title', 'slug'], condition=Q(status='PB'), name='%(app_label)s_%(class)s_unique_name_slug'),
+            models.CheckConstraint(check=Q(views__gte=0, likes__gte=0), name="%(app_label)s_%(class)s_stats_positive"),
         ]
         indexes = [
             models.Index(fields=['status', 'title']),
@@ -105,10 +107,10 @@ class Post(SoftDeleteMixin, models.Model):
         return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse_lazy("blog:post_detail", args=[self.slug])
+        return reverse_lazy("blog:article_detail", args=[self.slug])
     
 class Comment(SoftDeleteMixin, models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='blog_comments', on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
     is_approved = models.BooleanField(default=False, db_index=True)
@@ -118,4 +120,4 @@ class Comment(SoftDeleteMixin, models.Model):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'Comment by {self.user.username} on {self.post.title}'
+        return f'Comment by {self.user.username} on {self.article.title}'
