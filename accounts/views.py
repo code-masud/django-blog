4275a7ba70b9901d242b3a10655cffa17cacc1c3
code-masud <code.masud@hotmail.com>
2026-02-18 22:51:53 +0600

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,11 +3,11 @@ from django.views import generic
 from django.contrib import messages
 from .forms import ContactForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class AccountsProfileView(generic.TemplateView):
+class AccountsProfileView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'accounts/profile.html'
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Profile | {self.request.user.username}' 
