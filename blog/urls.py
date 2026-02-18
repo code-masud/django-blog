ccb1f1b846c0e66ab974431ba182132c67b34109
code-masud <code.masud@hotmail.com>
2026-02-18 11:42:26 +0600

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,22 +5,17 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # Home and list views
     path('', views.ArticleListView.as_view(), name='home'),
     path('blog/', views.ArticleListView.as_view(), name='article_list'),
-    path('blog/category/<slug:category_slug>/', 
-         views.ArticleListView.as_view(), name='category'),
-    path('blog/tag/<slug:tag_slug>/', 
-         views.ArticleListView.as_view(), name='tag'),
+    path('blog/search/', views.SearchResultView.as_view(), name='search'),
+    path('blog/<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('blog/category/<slug:category_slug>/', views.CategoryListView.as_view(), name='category'),
+    path('blog/tag/<slug:tag_slug>/', views.TagListView.as_view(), name='tag'),
+    path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail'),
     
     # Detail views
-    path('blog/<slug:slug>/', 
-         views.ArticleDetailView.as_view(), name='article_detail'),
-    path('blog/<slug:slug>/comment/', 
-         views.CommentCreateView.as_view(), name='add_comment'),
+#     path('blog/<slug:slug>/comment/', views.CommentCreateView.as_view(), name='add_comment'),
     
     # Author and archive views
     # path('authors/', views.AuthorListView.as_view(), name='author_list'),
-    # path('authors/<int:pk>/', 
-    #      views.AuthorDetailView.as_view(), name='author_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-# blog/views.py
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
@@ -8,90 +7,59 @@ from .models import Article, Category, Tag, Comment
 from .forms import CommentForm
 
 class ArticleListView(generic.ListView):
-    """Homepage with paginated blog posts"""
     model = Article
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
+    template_name = 'blog/article_list.html'
     paginate_by = 6
-    
-    # def get_queryset(self):
-    #     """Return published posts, optionally filtered by category/tag"""
-    #     queryset = Article.objects.filter(is_published=True, published_at__isnull=False)
-        
-    #     # Filter by category if specified
-    #     category_slug = self.kwargs.get('category_slug')
-    #     if category_slug:hil
-    #         category = get_object_or_404(Category, slug=category_slug)
-    #         queryset = queryset.filter(categories=category)
-        
-    #     # Filter by tag if specified
-    #     tag_slug = self.kwargs.get('tag_slug')
-    #     if tag_slug:
-    #         tag = get_object_or_404(Tag, slug=tag_slug)
-    #         queryset = queryset.filter(tags=tag)
-        
-    #     # Search functionality
-    #     query = self.request.GET.get('q')
-    #     if query:
-    #         queryset = queryset.filter(
-    #             Q(title__icontains=query) |
-    #             Q(content__icontains=query) |
-    #             Q(excerpt__icontains=query)
-    #         )
-        
-    #     return queryset.order_by('-published_at')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        context['tags'] = Tag.objects.all()
-        context['featured_posts'] = Article.objects.filter(status=Article.Status.PUBLISHED)[:3]
+        context['title'] = 'My Blog | Home'
         return context
 
 class ArticleDetailView(generic.DetailView):
-    """Individual blog post with comments"""
     model = Article
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
+    template_name = 'blog/article_detail.html'
     
     def get_queryset(self):
-        """Ensure only published posts are accessible"""
-        return Article.objects.filter(is_published=True, published_at__isnull=False)
+        return Article.objects.filter(status=Article.Status.PUBLISHED, published_at__isnull=False)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.filter(is_approved=True)
-        context['comment_form'] = CommentForm()
-        
-        # Related posts by category
-        categories = self.object.categories.all()
-        if categories:
-            context['related_posts'] = Article.objects.filter(
-                is_published=True,
-                categories__in=categories
-            ).exclude(id=self.object.id).distinct()[:3]
-        
+        context['titile'] = 'f{self.object.title} | Detail' 
         return context
 
-class CommentCreateView(LoginRequiredMixin, generic.CreateView):
-    """Handle comment creation (login required) [citation:9]"""
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/add_comment.html'
-    
-    def form_valid(self, form):
-        """Associate comment with logged-in user and blog post"""
-        form.instance.user = self.request.user
-        form.instance.post = get_object_or_404(Article, 
-            slug=self.kwargs['slug'], 
-            is_published=True
-        )
-        return super().form_valid(form)
-    
-    def get_success_url(self):
-        return reverse_lazy('blog:post_detail', args=[self.kwargs['slug']])
+class AuthorDetailView(generic.DetailView):
+    model = Article
+    template_name = 'blog/author_detail.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Article, slug=self.kwargs['slug'])
+        context['titile'] = 'f{self.object.title} | Author' 
+        return context
+
+class CategoryListView(generic.ListView):
+    model = Category
+    template_name = 'blog/category_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'My Blog | Category'
+        return context
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = 'blog/tag_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'My Blog | Tag'
+        return context
+
+class SearchResultView(generic.ListView):
+    model = Article
+    template_name = 'blog/search_result_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'My Blog | Search Result'
         return context
